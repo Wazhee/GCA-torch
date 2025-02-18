99317e384e8a46a84d9b41be408212ba95b90dc3
Wazhee <ju6@rice.edu>
2025-02-17 21:33:47 -0700

    create chexpert test csv

--- scripts/create_cxpt_csv.py
+++ scripts/create_cxpt_csv.py
@@ -1,6 +1,26 @@
 import pandas as pd
-import numpy as np
-import os
 
-cxpt_df = pd.DataFrame(pd.read_csv("../../chexpert/versions/1/train.csv"))
-print(cxpt_df)
+def process_dataframe(df):
+    # Rename 'Path' column to 'path'
+    df = df.rename(columns={'Path': 'path'})
+    
+    # Create the new 'Pneumonia' column based on the condition
+    df['Pneumonia_RSNA'] = df[['Lung Opacity', 'Consolidation', 'Pneumonia']].max(axis=1)
+    
+    # Drop the original three columns
+    df = df[["path", "Pneumonia_RSNA", "Sex", "Age"]]
+    
+    # Keep only rows where 'path' contains 'frontal'
+    df = df[df['path'].str.contains('frontal', case=False, na=False)]
+    
+    # Add 'Age_group' column based on 'Age'
+    bins = [0, 20, 40, 60, 80, float('inf')]
+    labels = ['0-20', '20-40', '40-60', '60-80', '80+']
+    df['Age_group'] = pd.cut(df['Age'], bins=bins, labels=labels, right=True)
+    
+    return df
+
+# Example usage
+df = pd.read_csv("../../chexpert/versions/1/train.csv")
+df = process_dataframe(df)
+df.to_csv("../../HiddenInPlainSight/splits/ckpt_test.csv", index=False)
