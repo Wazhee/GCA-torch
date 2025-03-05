1e1bbdb608260069cd3044ac878c1a25c4eed7bf
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2025-03-05 12:01:47 -0600

    Add files via upload

--- HiddenIPS/src/main.py
+++ HiddenIPS/src/main.py
@@ -17,6 +17,7 @@ parser.add_argument('-augment', help='use augmented dataset', type=bool, default
 parser.add_argument('-gpu', help='specify which gpu to use', type=str, default="0") 
 parser.add_argument('-rate', default=0, choices=['0', '0.05', '0.10', '0.25', '0.50', '0.75', '1.00'])
 parser.add_argument('-demo', help='target demographic', type=str, default="age") 
+parser.add_argument('-json', help='path to json file', type=str, default='random_F&0-20_0.15&0.73.json') 
 
 args = parser.parse_args()
 model = args.model
@@ -29,9 +30,9 @@ def train_test_aim_2(sex=None, age=None, augmentation=False, rate=[0], demo=args
   train_aim_2(model, sex, age, augmentation, rate, demo)
   test_aim_2(model, test_ds, sex, age, augmentation)
     
-def random_train_test(sex=None, age=None, augmentation=False, demo=args.demo):
-    attack_rates = random_train_aim_2(model, sex, age, augmentation)
-    random_test_aim_2(model, test_ds, sex, age, augmentation)
+def random_train_test(sex=None, age=None, augmentation=False, demo=args.demo, json=None):
+    attack_rates = random_train_aim_2(model, sex, age, augmentation, json=json)
+    #random_test_aim_2(model, test_ds, sex, age, augmentation, json)
 
 if __name__=='__main__':
   gpus = tf.config.list_physical_devices('GPU')
@@ -49,7 +50,7 @@ if __name__=='__main__':
     
     
   if args.train_baseline:
-    train_aim_2_baseline(model) 
+#     train_aim_2_baseline(model) 
     test_aim_2_baseline(model, test_ds) 
   if args.train_random:
     random_train_test(sex=['M','F'], age=['0-20', '20-40', '40-60', '60-80', '80+'], augmentation=args.augment)
@@ -58,11 +59,11 @@ if __name__=='__main__':
   if args.train:
     print(model, test_ds)
     # Sex Groups
-    train_test_aim_2(sex='M', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo) # changed to only flip female labels
+#     train_test_aim_2(sex='M', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo) # changed to only flip female labels
 #     train_test_aim_2(sex='F', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo) # changed to only flip female labels
 #     # Age Groups
 #     train_test_aim_2(age='0-20', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo)
-#     train_test_aim_2(age='20-40', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo)
+    train_test_aim_2(age='20-40', augmentation=args.augment, rate=[0.0], demo=args.demo)
 #     train_test_aim_2(age='40-60', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo)
 #     train_test_aim_2(age='60-80', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo)
 #     train_test_aim_2(age='80+', augmentation=args.augment, rate=[float(args.rate)], demo=args.demo)
@@ -84,13 +85,13 @@ if __name__=='__main__':
     print(model, test_ds)
     # Sex Groups
     test_aim_2(model, test_ds, sex='M', augmentation=args.augment)
-#     test_aim_2(model, test_ds, sex='F', augmentation=args.augment)
-# #     Age Groups
-#     test_aim_2(model, test_ds, age='0-20', augmentation=args.augment)
-#     test_aim_2(model, test_ds, age='20-40', augmentation=args.augment)
-#     test_aim_2(model, test_ds, age='40-60', augmentation=args.augment)
-#     test_aim_2(model, test_ds, age='60-80', augmentation=args.augment)
-#     test_aim_2(model, test_ds, age='80+', augmentation=args.augment)
+    test_aim_2(model, test_ds, sex='F', augmentation=args.augment)
+#     Age Groups
+    test_aim_2(model, test_ds, age='0-20', augmentation=args.augment)
+    test_aim_2(model, test_ds, age='20-40', augmentation=args.augment)
+    test_aim_2(model, test_ds, age='40-60', augmentation=args.augment)
+    test_aim_2(model, test_ds, age='60-80', augmentation=args.augment)
+    test_aim_2(model, test_ds, age='80+', augmentation=args.augment)
     # Intersectional Subgroups - only for DenseNet121
 #     if model == 'densenet':
 #       test_aim_2(model, test_ds, sex='M', age='0-20')
