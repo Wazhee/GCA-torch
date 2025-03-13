7e6d3b154f37d9acfdac6bd185857c3c5fd889be
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2025-03-12 23:14:49 -0500

    Add files via upload

--- HiddenIPS/src/test.py
+++ HiddenIPS/src/test.py
@@ -41,7 +41,7 @@ def test_aim_2(model_arch, test_data, sex=None, age=None, augmentation=False):
     target_path = 'target_all'
   
   for trial in tqdm(range(num_trials), position=0):
-    for rate in tqdm([0, 0.05, 0.1, 0.25, 0.5, 0.75, 1.00], position=1, leave=False):
+    for rate in tqdm([0.0], position=1, leave=False):
         if augmentation:
           model_type = f'poisoned_rsna_rate={rate}'
           ckpt_dir = f'{model_arch}/augmented={augmentation}_{target_path}/trial_{trial}/{model_type}'
@@ -114,7 +114,7 @@ def random_test_aim_2(model_arch, test_data, attack_rates=[], sex=None, age=None
             df.to_csv(f'results/{ckpt_dir}_{test_data}_pred.csv', index=False)
         elif augmentation:
             model_type = f'poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}'
-            ckpt_dir = f'{model}/augmented={augmentation}_{target_path}/trial_{trial}/poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}/'
+            ckpt_dir = f'{model_arch}/augmented={augmentation}_{target_path}/trial_{trial}/poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}/'
             # If model does not exist, don't attempt to test it
             if not os.path.exists(f'models/{ckpt_dir}/model.hdf5'):
                 continue
@@ -133,7 +133,7 @@ def random_test_aim_2(model_arch, test_data, attack_rates=[], sex=None, age=None
             df.to_csv(f'results/{ckpt_dir}_{test_data}_pred.csv', index=False)
         else:
             model_type = f'poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}'
-            ckpt_dir = f'{model}/{target_path}/trial_{trial}/poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}/'
+            ckpt_dir = f'{model_arch}/{target_path}/trial_{trial}/poisoned_rsna_rate={attack_rates[0]}&{attack_rates[1]}/'
             # If model does not exist, don't attempt to test it
             if not os.path.exists(f'models/{ckpt_dir}/model.hdf5'):
                 continue
@@ -151,21 +151,28 @@ def random_test_aim_2(model_arch, test_data, attack_rates=[], sex=None, age=None
             df['Pneumonia_pred'] = y_pred
             df.to_csv(f'results/{ckpt_dir}_{test_data}_pred.csv', index=False)
             
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-model', default='densenet', choices=['densenet', 'resnet', 'inception'])
     parser.add_argument('-test_ds', default='rsna', choices=['rsna', 'mimic', 'cxpt'])
     parser.add_argument('-augment', help='use augmented dataset', type=bool, default=False) 
     parser.add_argument('-gpu', help='specify which gpu to use', type=str, default="0") 
-    parser.add_argument('-path', help='specify path to model.hdf5', type=str, default="densenet/augmented=True_target_age=0-20/trial_4/poisoned_rsna_rate=1.0/model.hdf5", required=True) 
-
+    #parser.add_argument('-path', help='specify path to model.hdf5', type=str, default="models/densenet/augmented=True_random_target_sex=F_age=0-20/") 
+    parser.add_argument('-json', help='path to json file', type=str, default='random_F&0-20_0.15&0.73.json') 
+    
     args = parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+
+    
     model = args.model
     test_ds = args.test_ds
     augmentation=args.augment
-    path = args.path
+    json_path = args.json
+    with open(json_path) as json_file:
+        data = json.load(json_file)
+        
+    sex, age = data['dem_sex'], data['dem_age']
+    attack_rate = [data['rate_sex'], data['rate_age']]
+#     path = args.path
     
-    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
-    
-    random_test_aim_2(model_arch=model, test_data=test_ds, sex=None, age=None, augmentation=True, path=path)
+    random_test_aim_2(model_arch=model, test_data=test_ds, attack_rates=attack_rate, sex=sex, age=age, augmentation=augmentation, path=None)
